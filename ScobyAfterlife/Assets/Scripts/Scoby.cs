12a506660093b3d9b8c0aac75f57415628d5fb31
Morgaine-Jones <47799065+Morgaine-Jones@users.noreply.github.com>
2020-05-02 00:07:39 +0100

    Scoby light flashes when mouse hovers over

--- ScobyAfterlife/Assets/Scripts/Scoby.cs
+++ ScobyAfterlife/Assets/Scripts/Scoby.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Scoby : MonoBehaviour
 {
-    float duration = 1;
+    float duration = 0.1f;
     Color color0 = Color.red;
     Color color1 = Color.blue;
 
     private void OnMouseOver()
     {
         float t = Mathf.PingPong(Time.time, duration) / duration;
-        //light.color = Color.Lerp(color0, color1, t);
+        GetComponent<Light>().color = Color.Lerp(color0, color1, t);
     }
 }
