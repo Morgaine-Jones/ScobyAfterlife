00fe8902f33213e346075a5dfc64a504b0221062
Morgaine-Jones <47799065+Morgaine-Jones@users.noreply.github.com>
2020-05-01 22:42:47 +0100

    Art assets added and player move script added

---
+++ ScobyAfterlife/Assets/Scripts/Scoby.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoby : MonoBehaviour
+{
+    float duration = 1;
+    Color color0 = Color.red;
+    Color color1 = Color.blue;
+
+    private void OnMouseOver()
+    {
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        //light.color = Color.Lerp(color0, color1, t);
+    }
+}
