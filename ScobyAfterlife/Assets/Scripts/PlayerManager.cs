fd8d91906de7c7fd07eefa9d93af43238c6ba267
Morgaine-Jones <47799065+Morgaine-Jones@users.noreply.github.com>
2020-05-04 23:53:55 +0100

    Player movement and animations created

--- ScobyAfterlife/Assets/Scripts/PlayerManager.cs
+++ ScobyAfterlife/Assets/Scripts/PlayerManager.cs
@@ -4,34 +4,91 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public float speed = 1.5f;
+    Rigidbody2D rb;
 
-    void Start()
+    float moving = 0f;
+    bool jumping = false;
+
+    public float speed;
+    public float jumpHeight;
+
+    public bool onGround = false;
+    [SerializeField] private Transform GroundCheck;
+    bool isFacingRight = true;
+
+    [SerializeField] float JumpForce = 10;
+    [Range(0, .3f)] [SerializeField] float MovementSmoothing = 0.05f;
+    [SerializeField] bool AirControl = false;
+    Vector3 Velocity = Vector3.zero;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        Movement();
+        moving = Input.GetAxisRaw("Horizontal") * speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumping = true;
+            Debug.Log("hello1");
+        }
+    }
+    void FixedUpdate()
+    {
+        Move(moving * Time.fixedDeltaTime, jumping);
+        jumping = false;
+        Debug.Log("hello2");
     }
 
-    void Movement()
+    void Move(float move, bool jump)
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (onGround || AirControl)
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref Velocity, MovementSmoothing);
+
+            // Flips the player
+            if (move > 0 && !isFacingRight)
+            {
+                Flip();
+            }
+            else if (move < 0 && isFacingRight)
+            {
+                Flip();
+            }
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+
+        // Checks if should jump or not
+        if (onGround && jump)
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            onGround = false;
+            rb.AddForce(new Vector2(0f, JumpForce));
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+    }
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
         {
-            transform.position += Vector3.up * speed * Time.deltaTime;
+            onGround = true;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        else onGround = false;
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
         {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+            onGround = false;
         }
     }
 }
