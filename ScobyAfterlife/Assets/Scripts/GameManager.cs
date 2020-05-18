fb0305bf7ad2b1d2464397f7c0473a5a42da69ba
Morgaine-Jones <47799065+Morgaine-Jones@users.noreply.github.com>
2020-05-18 22:15:00 +0100

    clouds animated and game manager created

---
+++ ScobyAfterlife/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Variables
+    public GameObject Scoby1;
+    public GameObject Scoby2;
+    public GameObject Scoby3;
+    bool scobyDead = false;
+    int scobiesDead = 0;
+
+    // Start & Updates
+    void Update()
+    {
+        CheckScoby();
+
+        if (scobiesDead >= 3) 
+        {
+            GameOver();
+        }
+    }
+
+    void CheckScoby() 
+    {
+        scobyDead = Scoby1.GetComponent<Scoby>().scobyAlive;
+        if (scobyDead == true) 
+        {
+            scobiesDead = scobiesDead + 1;
+            scobyDead = false;
+        }
+
+        scobyDead = Scoby2.GetComponent<Scoby>().scobyAlive;
+        if (scobyDead == true)
+        {
+            scobiesDead = scobiesDead + 1;
+            scobyDead = false;
+        }
+
+        scobyDead = Scoby3.GetComponent<Scoby>().scobyAlive;
+        if (scobyDead == true)
+        {
+            scobiesDead = scobiesDead + 1;
+            scobyDead = false;
+        }
+    }
+
+    void GameOver() 
+    {
+        Application.Quit();
+    }
+}
